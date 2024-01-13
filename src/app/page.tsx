9ca89937b93d7665ce2ae19bfc8565e0f105883d
hyeonho cho <111675679+aesthetics-of-record@users.noreply.github.com>
2024-01-13 16:27:39 +0900

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      안녕
+      <Button>버튼</Button>
+      <ModeToggle />
+    </div>
+  );
+}
